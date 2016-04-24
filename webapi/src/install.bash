89b780db8f05f552acce9262ce92810cfc62b671
Nick Jones <njhazelh@gmail.com>
2016-04-24 00:30:09 -0700

    resources relative to script

--- webapi/src/install.bash
+++ webapi/src/install.bash
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-cp ../conf/django.conf /etc/nginx/sites-enabled
-cp ../conf/gunicorn.conf /etc/init/
+BASEDIR = $(dirname "$0")
+
+cp $BASEDIR/../conf/django.conf /etc/nginx/sites-enabled
+cp $BASEDIR/../conf/gunicorn.conf /etc/init/
 service gunicorn restart
