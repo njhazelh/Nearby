8f315cf1c3ebbad360bbc0b4b83eb480d3e1f8c6
Nick Jones <njhazelh@gmail.com>
2016-04-23 20:03:19 -0700

    Add old db settings

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'svqVUoATZq',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
