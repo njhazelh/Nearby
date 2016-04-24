c6113889746e8c32766b2e28b063648c296651f3
Nick Jones <njhazelh@gmail.com>
2016-04-24 00:56:30 -0700

    Also restart nginx after install

--- webapi/src/install.bash
+++ webapi/src/install.bash
@@ -3,4 +3,5 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 cp "$SCRIPT_DIR/../conf/django.conf" /etc/nginx/sites-enabled
 cp "$SCRIPT_DIR/../conf/gunicorn.conf"  /etc/init/
+service nginx restart
 service gunicorn restart
