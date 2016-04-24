58722a24b68bbf81c34d85691e6392bfe2fa517f
Nick Jones <njhazelh@gmail.com>
2016-04-24 00:54:20 -0700

    Rm unneeded echos

--- webapi/src/install.bash
+++ webapi/src/install.bash
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-echo "${BASH_SOURCE[0]}"
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $SCRIPT_DIR
-
 cp "$SCRIPT_DIR/../conf/django.conf" /etc/nginx/sites-enabled
 cp "$SCRIPT_DIR/../conf/gunicorn.conf"  /etc/init/
 service gunicorn restart
