5855f74d5e648e23c41d83bf7e0d94010bdfc33e
Phaelyn Kotuby <kotubyp@ccs.neu.edu>
2016-04-27 00:45:38 -0400

    Fixed shared pref issue!

--- android/Nearby/app/src/main/java/com/fake_nearby/nearby/nearby/SettingsActivity.java
+++ android/Nearby/app/src/main/java/com/fake_nearby/nearby/nearby/SettingsActivity.java
@@ -1,18 +1,48 @@
 package com.fake_nearby.nearby.nearby;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        System.out.println("shared pref changed!");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        try {
+            switch(key) {
+                case "firstlast":
+                    ApiRequests.doDisplayNameRequest();
+                    break;
+                case "username":
+                    ApiRequests.doAuthRequest(prefs.getString("username", "messedup"), prefs.getString("password", "messedup"));
+                    break;
+                case "password":
+                    ApiRequests.doAuthRequest(prefs.getString("username", "messedup"), prefs.getString("password", "messedup"));
+                    break;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong in API");
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragment {
